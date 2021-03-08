d70a0de294e69b87f74b990c3274c9e1d44b261a
unknown <anselmosuarez16@gmail>
2021-03-08 09:07:16 -0600

    Update

--- string_stringbuffer_stringbuilder/src/eg/StringMaxMin.java
+++ string_stringbuffer_stringbuilder/src/eg/StringMaxMin.java
@@ -13,20 +13,29 @@ public class StringMaxMin {
 		String s = sc.nextLine();
 
 		String ar[] = s.split(" ");
-		String max="";
-		String min="";
+		String max = "";
+		String min = "";
 		StringBuilder sb = new StringBuilder();
+		
 		for (int i = 0; i < ar.length; i++) {
-			String temp =ar[i]; 
-			if (i==0) {
-				max=ar[i]; 
-				
-			} 
+		
 			
-			
-			else if (ar[i].length()) {
-				
+			if (i == 0) {
+				max = ar[i];
+				min = ar[i];
+
 			}
+
+			if (ar[i].length() >= max.length()) {
+				max = ar[i];
+
+			}
+			
+			if (ar[i].length() <= min.length()) {
+				min = ar[i];
+
+			}
+			
 
 		}
 		System.out.println("The longest word is " + max);
